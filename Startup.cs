fd183dd5b2d13b9e33f9b1f09c544cf8df681e33
Nikolas Pateras <nikolas.98@live.com>
2022-09-20 03:19:08 +0300

    Initial Commit.

---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+namespace MakingHttpRequest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpClient<IWeatherService, WeatherService>(c =>
+            {
+                c.BaseAddress = new Uri("http://api.weatherapi.com/v1/current.json");
+            }).AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2)))
+                .AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(5, TimeSpan.FromSeconds(5)));
+
+            // Alternatively you can also add extension methods like below to keep this method clean
+            // services.AddWeatherService();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ WeatherService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MakingHttpRequest
+{
+    public interface IWeatherService
+    {
+        Task<string> Get(string cityName);
+    }
+
+    public class WeatherService : IWeatherService
+    {
+        private HttpClient _httpClient;
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Get(string cityName)
+        {
+            var apiKey = "2f88f13a8557410693e193741221909";
+
+            string APIURL = $"?key={apiKey}&q={cityName}";
+            var response = await _httpClient.GetAsync(APIURL);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
